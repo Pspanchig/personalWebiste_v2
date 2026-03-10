033599f59b0bae3e85be491d266dcd0ba3b98106
pspanchig <pspanchig@gmail.com>
2026-03-10 12:31:44 -0500

    Restore Pages deployment with hash routing

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,5 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { provideHttpClient } from '@angular/common/http';
 import { provideRouter, withInMemoryScrolling } from '@angular/router';
 
@@ -8,6 +9,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideHttpClient(),
+    { provide: LocationStrategy, useClass: HashLocationStrategy },
     provideRouter(
       routes,
       withInMemoryScrolling({
